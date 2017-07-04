912be2cb74683fa260b1769ef4acd054fcba0728
Aseef Ahmed <aseefahmed@gmail.com>
2017-07-04 10:37:43 +0600

    4

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use App\News;
 use App\Tag;
+use App\LatestNews;
 use App\NewsType;
 
 use App\Http\Requests;
@@ -17,7 +18,7 @@ class NewsController extends Controller
     {
     	if(Auth::user()->role == 1)
     	{
-    			$data['news_list'] = DB::table('news')->paginate(10);
+    			$data['news_list'] = DB::table('news')->orderBy('id', 'desc')->paginate(10);
     	}
     	else if(Auth::user()->role == 2)
     	{
@@ -28,11 +29,11 @@ class NewsController extends Controller
 				{
 					array_push($cat_arr, $cat->id);
 				}
-				$data['news_list'] = DB::table('news')->join('news_types', 'news_types.news_id', '=', 'news.id')->whereIn('news_types.category_id', $cat_arr)->select('news.*')->paginate(10);
+				$data['news_list'] = DB::table('news')->join('news_types', 'news_types.news_id', '=', 'news.id')->whereIn('news_types.category_id', $cat_arr)->orderBy('id', 'desc')->select('news.*')->paginate(10);
     	}
     	else if(Auth::user()->role == 3)
     	{
-    			$data['news_list'] = DB::table('news')->where('created_by', Auth::user()->id)->paginate(10);
+    			$data['news_list'] = DB::table('news')->where('created_by', Auth::user()->id)->orderBy('id', 'desc')->paginate(10);
     	}
     	return view('news.index', $data);
     }
@@ -71,6 +72,25 @@ class NewsController extends Controller
 				$news_types->news_id = $news->id;
 				$news_types->category_id = $category;
 				$news_types->save();
+				
+				if($request->flag == 1){
+					$count = DB::table('latest_news')->where('category_id', $category)->count();
+					if($count < 4){
+						DB::table('latest_news')->insert([
+							'category_id' => $category,
+							'news_id' => $news->id
+						]);
+					}
+					else if($count == 4){
+						$latest_news = DB::table('latest_news')->where('category_id', $category)->orderBy('id', 'asc')->take(1)->get();
+						DB::table('latest_news')->where('id', $latest_news[0]->id)->delete();
+						DB::table('latest_news')->insert([
+							'category_id' => $category,
+							'news_id' => $news->id
+						]);
+				
+					}
+				}
 			}
 		}
 		if(count($request->tags) > 0 )
@@ -83,6 +103,7 @@ class NewsController extends Controller
 				$tags->save();
 			}
 		}
+		
 	}
 	
 	public function viewNews($id)
@@ -94,6 +115,7 @@ class NewsController extends Controller
 	public function removeNews($id)
 	{
 		DB::table('news')->where('id', $id)->delete();
+		DB::table('latest_news')->where('news_id', $id)->delete();
 	}
 	
 	public function editNews($id)
@@ -121,6 +143,7 @@ class NewsController extends Controller
 		$news->save();
 		DB::table('news_types')->where('news_id', $request->id)->delete();
 		DB::table('tags')->where('news_id', $request->id)->delete();
+		DB::table('latest_news')->where('news_id', $news->id)->delete();
 		if(count($request->categories) > 0 )
 		{
 			foreach($request->categories as $category)
@@ -129,6 +152,25 @@ class NewsController extends Controller
 				$news_types->news_id = $news->id;
 				$news_types->category_id = $category;
 				$news_types->save();
+				
+				if($request->flag == 1){
+					$count = DB::table('latest_news')->where('category_id', $category)->count();
+					if($count < 4){
+						DB::table('latest_news')->insert([
+							'category_id' => $category,
+							'news_id' => $news->id
+						]);
+					}
+					else if($count == 4){
+						$latest_news = DB::table('latest_news')->where('category_id', $category)->orderBy('id', 'asc')->take(1)->get();
+						DB::table('latest_news')->where('id', $latest_news[0]->id)->delete();
+						DB::table('latest_news')->insert([
+							'category_id' => $category,
+							'news_id' => $news->id
+						]);
+				
+					}
+				}
 			}
 		}
 		
@@ -142,6 +184,8 @@ class NewsController extends Controller
 				$tags->save();
 			}
 		}
+		
+		
 		
 		
 	}
